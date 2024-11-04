5ed0a762fbaa12c22f97778dfcea924f6fdedfbe
Bhuvan H.C <91734756+Imbobji@users.noreply.github.com>
2024-11-04 20:11:45 +0530

    Update App.java

--- Bankproj/src/main/java/com/bhuvan/App.java
+++ Bankproj/src/main/java/com/bhuvan/App.java
@@ -5,6 +5,6 @@ package com.bhuvan;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Bhuvan");
+        System.out.println("Hello iftheqar");
     }
 }
